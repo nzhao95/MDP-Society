ece0e1c63a3df75f279d037892b1c11f7870010b
nzhao95 <nicolas.zhao1@gmail.com>
2024-01-12 00:57:28 +0100

    Store position and satisfaction values in contiguous numpy arrays for future learning computations. Keep a reference to those values inside the Human class, be sure to not redefine the arrays but only change the elements inside otherwise they lose track of the original position.Compute time simulation of the world with a fix background and moving characters

--- main.py
+++ main.py
@@ -10,34 +10,42 @@ class World :
     _Forests = np.array([])
     _Lakes = np.array([])
     _HumanPositions = np.array([])
+    _HumanSatisfactions = np.array([])
 
     def __init__(self, world_size, humans, forests, lakes):
         self._Size =  world_size 
         self._Humans = humans
+        self._HumanPositions = np.array([human._Position for human in humans])# should be shared
+        self._HumanSatisfactions = np.array([human._Satisfaction for human in humans])
+        i = 0
+        for human in humans:
+            human._WorldIndex = i
+            human._Position = self._HumanPositions[i]
+            human._Satisfaction = self._HumanSatisfactions[i]
+            i += 1
         self._Forests = forests
         self._Lakes = lakes
-        self._HumanPositions = np.array([human._Position for human in self._Humans])
         
     def UpdateTime(self):
         for human in self._Humans:
             human.UpdateTime()
 
-    def Draw(self):
-
+    def InitDraw(self):
+        
         # Create a figure and axis
         fig, ax = plt.subplots()
 
         # Plot the world boundary
         ax.set_xlim(0, self._Size)
         ax.set_ylim(0, self._Size)
-        if (self._HumanPositions.shape[0] != 0):
-            for i in range(len(self._Humans)) : 
-                if self._Humans[i]._PositionDirty:
-                    self._HumanPositions[i] = self._Humans[i]._Position
 
-            # Plot characters as points
-            ax.scatter(self._HumanPositions[:, 0] , self._HumanPositions[:, 1], marker='o', color='red', label='Characters')
 
+        # Add labels
+        ax.set_title('MDP Simulated Society')
+        ax.legend()
+        return fig, ax
+
+    def DrawBackground(self, fig, ax):
         # Plot forests as rectangles
         for forest in self._Forests:
             rect = plt.Rectangle((forest[0], forest[1]), forest[2], forest[3], color='green', alpha=0.5)
@@ -47,14 +55,29 @@ class World :
         for lake in self._Lakes:
             circle = plt.Circle((lake[0], lake[1]), lake[2], color='blue', alpha=0.5)
             ax.add_patch(circle)
+            
+        background = fig.canvas.copy_from_bbox(ax.bbox)
+        return background
 
-        # Add labels
-        ax.set_title('MDP Simulated Society')
-        ax.legend()
-        plt.pause(0.5)  # Add a short pause to observe the updates
+    def DrawHumans(self, ax):
+        humanPoints = []
+        for humanPos in self._HumanPositions:
+            # Plot characters as points
+            humanPoints.append( ax.plot(humanPos[0] , humanPos[1], marker='o', color='red', label='Characters')[0])
+        return humanPoints
+    
+    def UpdateDraw(self, fig, ax, background, humanPoints):
+            # restore background
+            fig.canvas.restore_region(background)
 
-        # Show the plot
-        plt.show()
+            # redraw just the points
+            for point in humanPoints:
+                ax.draw_artist(point)
+
+            # fill in the axes rectangle
+            fig.canvas.blit(ax.bbox)
+
+
 
 HUMAN_NEEDS = []
 DEFAULT_SATISFACTION = []
@@ -62,9 +85,7 @@ POSSIBLE_ACTIONS = []
 DEATH_VALUE = -1e10
 
 class Human:
-    _Position = [0, 0]
-    _Satisfaction = dict()
-    _PositionDirty = False
+    _WorldIndex = -1
 
     def __init__(self, position, satisfaction = DEFAULT_SATISFACTION):
         self._Position = position
@@ -113,9 +134,9 @@ class Action:
 
     def Do(self, human):
         if self._Need:
-            human._Satisfaction[self._Need] = self._Result(human._Satisfaction[self._Need])
+            human._Satisfaction[self._Need][0], human._Satisfaction[self._Need][1] = self._Result(human._Satisfaction[self._Need])
         if self._EndPosition is not None:
-            human._Position = self._EndPosition(human._Position)
+            human._Position[0], human._Position[1] = self._EndPosition(human._Position)
             human._PositionDirty = True
 
     def Predict(self, human):
@@ -143,17 +164,30 @@ aPoop = Action(nPoo, lambda x : 100, lambda t : 3)
 
 POSSIBLE_ACTIONS = [aEat, aDrink, aSleep, aPoop]
 
-characters = [Human( [30, 20]), Human([50, 40])]
+characters = [Human( [30, 20],DEFAULT_SATISFACTION), Human([50, 40], DEFAULT_SATISFACTION)]
 
 # Define the positions of characters, forests, and lakes
 forests = np.array([[10, 10, 30, 40], [70, 40, 20, 20]])
 lakes = np.array([[40, 80, 5], [60, 30, 10]])
 
 myWorld = World(100, characters, forests, lakes)
-myWorld.Draw()
 
+print(myWorld._HumanPositions)
+print(myWorld._HumanSatisfactions)
+
+
+#Initial Draw
+fig, ax = myWorld.InitDraw()
+background = myWorld.DrawBackground(fig, ax)
+humanPoints = myWorld.DrawHumans(ax)
+print(humanPoints)
+plt.ion()
 for i in range(100):
     for human in myWorld._Humans : 
         human.DoAction(aMove)
-        
-    myWorld.Draw()
+        humanPoints[human._WorldIndex].set_data(human._Position[0] , human._Position[1])
+    myWorld.UpdateDraw(fig, ax, background, humanPoints)
+    plt.pause(0.033)  # Add a short pause to observe the updates
+
+plt.ioff()
+plt.show()
