441797a3baa92c08b80b2c2977bf42d812993904
Nzhao <nicolas.zhao1@gmail.com>
2024-01-14 23:42:03 +0100

    Move entities to a given sphere, probably need to rework the action to sotre a given action and perform it for a while

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class World :
         self._Forests = forests
         self._Lakes = lakes
         
-    def UpdateTime(self):
+    def UpdateWorld(self):
         for human in self._Humans:
             human.UpdateTime()
 
@@ -66,10 +66,12 @@ class World :
             humanPoints.append( ax.plot(humanPos[0] , humanPos[1], marker='o', color='red', label='Characters')[0])
         return humanPoints
     
-    def UpdateDraw(self, fig, ax, background, humanPoints):
+    def UpdateDraw(self, world, fig, ax, background, humanPoints):
             # restore background
             fig.canvas.restore_region(background)
 
+            for human in world._Humans :
+                humanPoints[human._WorldIndex].set_data(human._Position[0] , human._Position[1])
             # redraw just the points
             for point in humanPoints:
                 ax.draw_artist(point)
@@ -104,8 +106,8 @@ class Human:
     def PredictNextAction():
         return 
 
-    def DoAction(self, action):
-        action.Do(self)
+    def DoAction(self, action, world):
+        action.Do(self, world)
 
 
 class Need:
@@ -121,7 +123,7 @@ class Action:
     _Need = None
     _Result = lambda x : x
     _TimeNeed = lambda t : 1
-    _EndPosition = lambda pos : pos
+    _EndPosition = lambda pos, velocity, world : pos
 
     def __init__(self, need = None, result = None, timeNeeded = None, endPosition = None):
         self._Need = need
@@ -132,19 +134,52 @@ class Action:
         if endPosition is not None:
             self._EndPosition = endPosition
 
-    def Do(self, human):
+    def Do(self, human, world):
         if self._Need:
-            human._Satisfaction[self._Need][0], human._Satisfaction[self._Need][1] = self._Result(human._Satisfaction[self._Need])
+            need_index = HUMAN_NEEDS_INDICES[self._Need]
+            human._Satisfaction[need_index] = self._Result(human._Satisfaction[need_index])
         if self._EndPosition is not None:
-            human._Position[0], human._Position[1] = self._EndPosition(human._Position)
+            human._Position[0], human._Position[1] = self._EndPosition(human._Position, WALKING_VELOCITY, element_list = world._Lakes)
             human._PositionDirty = True
 
     def Predict(self, human):
         if self._Need:
-            return self._Result(human._Satisfaction[self._Need])
+            return self._Result(human._Satisfaction[HUMAN_NEEDS_INDICES[self._Need]])
         
     def TimeNeeded(self, human, world):
         return self._TimeNeed(world._CurentTime)
+
+def FindNearest(pos, element_list):
+    if (len(element_list) == 0):
+        return None
+    elif (len(element_list) == 1):
+        return element_list[0]
+    nearest_element = element_list[0]
+    min_dist = max(0, np.linalg.norm(pos - element_list[0][0:2]) - element_list[0][2])
+    for element in element_list[1:]: 
+        dist = max(0, np.linalg.norm(pos - element[0:2]) - element[2])
+        if dist < min_dist:
+            min_dist = dist
+            nearest_element = element
+    return nearest_element
+
+def GoToCircleEdge(pos, vel, element_list):
+    destination = FindNearest(pos, element_list)
+    dest_pos = destination[:2]
+    dist = np.linalg.norm(dest_pos - pos)
+    direction = (dest_pos - pos) / np.linalg.norm(dest_pos - pos)
+    if (dist > destination[2]):
+        return pos + (vel * direction)
+    return pos
+
+def GoToSquareEdge(pos, vel, element_list):
+    destination = FindNearest(pos, element_list)
+    dest_pos = destination[:2] + destination[3:] / 2
+    vec = dest_pos - pos
+    direction = (vec) / np.linalg.norm(vec)
+    if (abs(vec[0]) > destination[2] / 2 or abs(vec[1]) > destination[3] /2):
+        return pos + (vel * direction)
+    return pos
 
 # Make Needs
 nFood = Need(lambda x : (50-x) * 2 if x >= 0 else DEATH_VALUE, lambda x : x - 1)
@@ -152,41 +187,40 @@ nWater = Need(lambda x : (50-x) * 4 if x >= 0 else DEATH_VALUE, lambda x : x - 2
 nRest = Need(lambda x : (50-x), lambda x : x - 1)
 nPoo = Need(lambda x : (50-x) / 2, lambda x : x - 1)
 HUMAN_NEEDS = [nFood, nWater, nRest, nPoo]
-DEFAULT_SATISFACTION = [100, 100, 100, 100]
+HUMAN_NEEDS_INDICES = dict()
+for index in range(len(HUMAN_NEEDS)):
+    HUMAN_NEEDS_INDICES[HUMAN_NEEDS[index]] = index
 
+DEFAULT_SATISFACTION = [100, 100, 100, 100]
+WALKING_VELOCITY = 1.0
 
 # Make Actions 
-aMove = Action(None, None, None, lambda x : [x[0]+1, x[1]])
+aMove = Action(None, None, None, lambda x, v, w : [x[0]+v, x[1]])
 aEat = Action(nFood, lambda x : 100, lambda t : 3)
-aDrink = Action(nWater, lambda x : 100, lambda t : 3)
+aDrink = Action(nWater, lambda x : 100, lambda t : 3, GoToCircleEdge)
 aSleep = Action(nRest, lambda x : 100, lambda t : 3)
 aPoop = Action(nPoo, lambda x : 100, lambda t : 3)
 
 POSSIBLE_ACTIONS = [aEat, aDrink, aSleep, aPoop]
 
-characters = [Human( [30, 20],DEFAULT_SATISFACTION), Human([50, 40], DEFAULT_SATISFACTION)]
+characters = [Human( [30.0, 20.0],DEFAULT_SATISFACTION), Human([50.0, 40.0], DEFAULT_SATISFACTION)]
 
 # Define the positions of characters, forests, and lakes
-forests = np.array([[10, 10, 30, 40], [70, 40, 20, 20]])
-lakes = np.array([[40, 80, 5], [60, 30, 10]])
+forests = np.array([[10.0, 10.0, 30.0, 40.0], [70.0, 40.0, 20.0, 20.0]])
+lakes = np.array([[40.0, 80.0, 5.0], [60.0, 30.0, 10.0]])
 
 myWorld = World(100, characters, forests, lakes)
-
-print(myWorld._HumanPositions)
-print(myWorld._HumanSatisfactions)
-
 
 #Initial Draw
 fig, ax = myWorld.InitDraw()
 background = myWorld.DrawBackground(fig, ax)
 humanPoints = myWorld.DrawHumans(ax)
-print(humanPoints)
+
 plt.ion()
 for i in range(100):
     for human in myWorld._Humans : 
-        human.DoAction(aMove)
-        humanPoints[human._WorldIndex].set_data(human._Position[0] , human._Position[1])
-    myWorld.UpdateDraw(fig, ax, background, humanPoints)
+        human.DoAction(aDrink, myWorld)
+    myWorld.UpdateDraw(myWorld, fig, ax, background, humanPoints)
     plt.pause(0.033)  # Add a short pause to observe the updates
 
 plt.ioff()
